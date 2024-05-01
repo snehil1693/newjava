d815a16a0b9b3548dd6e2c112528bc4df4a0576c
snehil1693 <snehil1693@gmai.com>
2024-05-01 20:19:25 +0530

    Commit new java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.net.StandardSocketOptions;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hi I am snehil");
 
     }
 }
