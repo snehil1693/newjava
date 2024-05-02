3ea7d1a075d0188e616a565b96f0f919b710978b
snehil1693 <snehil1693@gmai.com>
2024-05-02 19:13:42 +0530

    Commit new literals

--- src/CWH_Ch1_PS.java
+++ src/CWH_Ch1_PS.java
@@ -1,4 +1,6 @@
-    public class CWH_Ch1_PS {
+import java.util.Scanner;
+
+public class CWH_Ch1_PS {
         public static void main(String[] args) {
 //            int a = 12;
 //            int b = 15;
@@ -7,14 +9,20 @@
 //            int sum = a+b+c;
 //            System.out.println(sum);
 
-              float subject1 = 45;
-              float subject2 = 95;
-              float subject3 = 48;
+//              float subject1 = 45;
+//              float subject2 = 95;
+//              float subject3 = 48;
+//
+//              float cgpa = (subject1 + subject2 + subject3)/30;
+//
+//            System.out.println(cgpa);
 
-              float cgpa = (subject1 + subject2 + subject3)/30;
+            System.out.println(" What is your name ?");
+            Scanner sc = new Scanner(System.in);
 
-            System.out.println(cgpa);
+            String name = sc.next();
 
+            System.out.println("Hello " + name + " How are you?");
 
 
 
