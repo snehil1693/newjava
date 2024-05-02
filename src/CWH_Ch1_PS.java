0da3c38d2d397aea3f81295f91e545113c4811bd
snehil1693 <snehil1693@gmai.com>
2024-05-02 19:08:33 +0530

    Commit new literals

---
+++ src/CWH_Ch1_PS.java
@@ -0,0 +1,22 @@
+    public class CWH_Ch1_PS {
+        public static void main(String[] args) {
+//            int a = 12;
+//            int b = 15;
+//            int c = 3;
+//
+//            int sum = a+b+c;
+//            System.out.println(sum);
+
+              float subject1 = 45;
+              float subject2 = 95;
+              float subject3 = 48;
+
+              float cgpa = (subject1 + subject2 + subject3)/30;
+
+            System.out.println(cgpa);
+
+
+
+
+        }
+    }
