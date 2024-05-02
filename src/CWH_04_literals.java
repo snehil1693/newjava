540986b9a480142d727b27bc811c8f57a247613b
snehil1693 <snehil1693@gmai.com>
2024-05-02 13:11:08 +0530

    Commit new literals

--- src/CWH_04_literals.java
+++ src/CWH_04_literals.java
@@ -1,7 +1,13 @@
 
 public class CWH_04_literals {
     public static void main(String[] args){
-        int age = 34;
+        byte age = 34;
+        int age2 = 56;
+        long ageDino = 566666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.660;
         System.out.println(age);
+
     }
 }
